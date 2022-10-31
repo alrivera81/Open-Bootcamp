ea86209b4c7aa9ad911de6d93e718955522b2f12
Alvaro Rivera <alvaroriveragiraldo@hotmail.com>
2022-10-31 11:40:37 -0500

    Commit Inicial del Curso Ejercicio 1

---
+++ EjerciciosTema1/src/com/practice/EjercicioTema1.java
@@ -0,0 +1,31 @@
+
+package com.practice;
+
+
+public class EjercicioTema1 {
+    public static void main(String[] args) {
+        byte varByte = 8;
+        short varShort = 14;
+        int varEntero = 125;
+        long varLong = 1250;
+        float varFloat = 8.3f;
+        double varDouble = 15.5d;
+        boolean varBool = false;
+        char varChar = 'v';
+        String varString = "Hello OpenBootcamp";
+        System.out.println("Tipo byte = " + varByte);
+        System.out.println("Tipo short = " + varShort);
+        System.out.println("Tipo int = " + varEntero);
+        System.out.println("Tipo long = " + varLong);
+        System.out.println("Tipo float = " + varFloat);
+        System.out.println("Tipo double = " + varDouble);
+        System.out.println("Tipo boolean = " + varBool);
+        System.out.println("Tipo char = " + varChar);
+        System.out.println("Tipo String = " + varString);
+        
+        
+        
+        
+        
+    }
+}
