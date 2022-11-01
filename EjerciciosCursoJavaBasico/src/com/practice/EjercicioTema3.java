a5e61e0eef97023fd6886ab1fa9887f79f40f483
Alvaro Rivera <alvaroriveragiraldo@hotmail.com>
2022-11-01 10:43:18 -0500

    Commit Entrega ejercicio practico 3

---
+++ EjerciciosCursoJavaBasico/src/com/practice/EjercicioTema3.java
@@ -0,0 +1,13 @@
+
+package com.practice;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        String [] nombres = {"Alvaro","Carolina","Isabella","Simon","Moka"};
+        int totalNombres = nombres.length;
+        for (int i = 0;i < totalNombres;i++){
+            
+            System.out.println(nombres[i]);
+        }
+    }
+}
